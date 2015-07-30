64a7b60f3ecf8f0c0aca3528d47c00998896985e
Amitai Burstein <amitai@gizra.com>
2015-07-30 21:04:43 +0300

    Shoovify repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # prometsource-shoov
+
+> Visual monitor Promet Source
+
+Files scaffolded with `yo shoov --base-url=https://www.prometsource.com`
---
+++ visual-monitor/test/tests.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var shoovWebdrivercss = require('shoov-webdrivercss');
+
+var projectName = 'prometsource';
+
+// This can be executed by passing the environment argument like this:
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=iphone5 mocha
+
+var capsConfig = {
+  'chrome': {
+    'project': projectName,
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    'project': projectName,
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  },
+  'iphone5': {
+    'project': projectName,
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'chromeOptions': {
+      'mobileEmulation': {
+        'deviceName': 'Apple iPhone 5'
+      }
+    }
+  }
+};
+
+var selectedCaps = process.env.SELECTED_CAPS || undefined;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+
+var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
+var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
+
+var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'https://www.prometsource.com';
+
+describe('Visual monitor testing', function() {
+
+  this.timeout(99999999);
+  var client = {};
+
+  before(function(done){
+    client = shoovWebdrivercss.before(done, caps);
+  });
+
+  after(function(done) {
+    shoovWebdrivercss.after(done);
+  });
+
+  it('should show the home page',function(done) {
+    client
+      .url(baseUrl + '/about')
+      .webdrivercss(testName + '.about', {
+        name: '1',
+        exclude: [
+          '.about-block-title',
+          // Team
+          '.view-user-profile'
+        ],
+        remove: []
+      }, shoovWebdrivercss.processResults)
+      .call(done);
+  });
+});
